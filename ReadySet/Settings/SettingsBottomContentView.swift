09cb2c7da44210e2a4ac3d1760bdf313c04ae34e
Nick Molargik <nick_molargik@sweetwater.com>
2024-04-16 21:13:36 -0400

    fix settings view, share URL

--- ReadySet/Settings/SettingsBottomContentView.swift
+++ ReadySet/Settings/SettingsBottomContentView.swift
@@ -14,14 +14,13 @@ struct SettingsBottomContentView: View {
     @AppStorage("userName") var username: String = ""
     @State var name : String = ""
     
-    let link = URL(string: "www.google.com")! //TODO: change this to our App listing
-    
     var body: some View {
-        VStack (spacing: 0) {
+        VStack {
+            Spacer()
+            
             Text("Set Your Name")
                 .font(.system(size: 18, weight: .semibold))
                 .lineLimit(1)
-                .padding(.bottom, 5)
             
             Text("(This will update when you leave Settings)")
                 .font(.system(size: 8, weight: .regular))
@@ -64,64 +63,50 @@ struct SettingsBottomContentView: View {
             .padding(.bottom, 20)
             .padding(.horizontal)
             
-            Spacer()
-            
-            Divider()
-                .padding(.horizontal)
-                .scaleEffect(y: 3)
-                .shadow(radius: 2, y: 2)
-                .padding(.bottom, 10)
-            
-            legalStack
-        }
-        .padding(.top, 20)
-    }
-    
-    private var legalStack: some View {
             HStack {
-                VStack(alignment: .center) {
-                    userText("Ready, Set", style: .title2)
-                    userText("2024, Nicholas Molargik", style: .caption)
-                    userText("Contributions from Nicholas Yoder", style: .caption)
+                VStack {
+                    Text("Ready, Set")
+                        .bold()
+                        .font(.title3)
+                    
+                    Text("2024, Nicholas Molargik")
+                        .bold()
+                        .font(.caption)
+                    
+                    Text("Contributions from Nicholas Yoder and Dante Maslin")
+                        .bold()
+                        .font(.caption)
                         .multilineTextAlignment(.center)
-                    userText("v1.0.0", style: .caption)
+                    
+                    Text("v1.0.0")
+                        .bold()
+                        .font(.caption)
                     
                     ShareLink(
-                        item: link,
-                        message: Text("Check out Ready, Set - a new fitness tracking app!"),
-                        preview: SharePreview(
-                            "Ready, Set | App Store",
-                            image: Image("icon")
-                        )
+                        item: URL(string: "https://apps.apple.com/app/id6484503374")!,
+                        subject: Text("Ready, Set"),
+                        message: Text("Check out Ready, Set - a new fitness trend tracking app!")
                     ) {
                         ZStack {
                             Capsule()
-                                .frame(width: 130, height: 30)
                                 .foregroundStyle(.ultraThickMaterial)
                             
                             Text("Share App")
                                 .bold()
                         }
                     }
+                    .frame(width: 100, height: 30)
                 }
-                .padding(.leading, 10)
-                .padding(.bottom)
-
-                Spacer()
-
+                
                 Image("icon")
                     .resizable()
                     .aspectRatio(contentMode: .fit)
-                    .frame(width: 150)
-                    .padding(.top, -10)
+                    .frame(height: 100)
             }
+            
+            Spacer()
         }
-
-        private func userText(_ text: String, style: Font) -> some View {
-            Text(text)
-                .bold()
-                .font(style)
-        }
+    }
 }
 
 #Preview {
